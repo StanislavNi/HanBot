15ad66d88004bce5469f41dd9b177c5641698679
StanislavNi <stanislavni1991@gmail.com>
2018-04-03 23:13:20 +0900

    changed

--- hangook_bot.py
+++ hangook_bot.py
@@ -1,32 +1,16 @@
 import config
 import telebot
 from googletrans import Translator
-from telebot import types
 
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=["start"])
-def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.row('Перевод на корейский', 'Перевод с корейского')
-    markup.row('Курс валют', 'Погода')
-    bot.send_message(message.chat.id, "Choose the command:",
-                     reply_markup=markup)
-    bot.register_next_step_handler(message, commands)
-
-
-def commands(message):
-    if message.text == 'Перевод на корейский':
-        translator = Translator()
-        translations = translator.translate([message.text], dest='ko')
-        for translation in translations:
-            bot.send_message(message.chat.id, translation.text)
-    elif message.text == 'Перевод с корейского':
-        translator = Translator()
-        translations = translator.translate([message.text], dest='ru')
-        for translation in translations:
-            bot.send_message(message.chat.id, translation.text)
+@bot.message_handler(content_types=["text"])
+def translate_all_messages(message):
+    translator = Translator()
+    translations = translator.translate([message.text], dest='ko')
+    for translation in translations:
+        bot.send_message(message.chat.id, translation.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
