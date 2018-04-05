e4b2a2bfe645c2f24c591f6c2b97bc588840a417
StanislavNi <stanislavni1991@gmail.com>
2018-04-05 23:52:59 +0900

    translation from korean added

--- hangook_bot.py
+++ hangook_bot.py
@@ -5,10 +5,25 @@ from googletrans import Translator
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(content_types=["text"])
-def translate_all_messages(message):
+@bot.message_handler(commands=['tokorean'])
+def input_messages(message):
+    sent = bot.send_message(message.chat.id, 'Введите текст')
+    bot.register_next_step_handler(sent, translate_to_korean)
+
+def translate_to_korean(message):
     translator = Translator()
     translations = translator.translate([message.text], dest='ko')
+    for translation in translations:
+        bot.send_message(message.chat.id, translation.text)
+
+@bot.message_handler(commands=['fromkorean'])
+def input_messages1(message):
+    sent = bot.send_message(message.chat.id, 'Введите текст')
+    bot.register_next_step_handler(sent, translate_from_korean)
+
+def translate_from_korean(message):
+    translator = Translator()
+    translations = translator.translate([message.text], dest='ru')
     for translation in translations:
         bot.send_message(message.chat.id, translation.text)
 
