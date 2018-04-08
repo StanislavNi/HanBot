f51f315380d5d6d44aaacf4429f457125c5406ac
StanislavNi <stanislavni1991@gmail.com>
2018-04-09 00:21:21 +0900

    buttons added

--- hangook_bot.py
+++ hangook_bot.py
@@ -1,11 +1,20 @@
 import config
 import telebot
+from telebot import types
 from googletrans import Translator
 
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['tokorean'])
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('/На_корейский', '/С_корейского')
+    markup.row('/Погода', '/Курс')
+    bot.send_message(message.chat.id, "Choose the command:",
+                     reply_markup=markup)
+
+@bot.message_handler(commands=['На_корейский'])
 def input_messages(message):
     sent = bot.send_message(message.chat.id, 'Введите текст')
     bot.register_next_step_handler(sent, translate_to_korean)
@@ -16,7 +25,7 @@ def translate_to_korean(message):
     for translation in translations:
         bot.send_message(message.chat.id, translation.text)
 
-@bot.message_handler(commands=['fromkorean'])
+@bot.message_handler(commands=['С_корейского'])
 def input_messages1(message):
     sent = bot.send_message(message.chat.id, 'Введите текст')
     bot.register_next_step_handler(sent, translate_from_korean)
